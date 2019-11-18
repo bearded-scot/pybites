785180ea2e4e545cece9346c29ed849423d60692
bearded-scot <32096683+bearded-scot@users.noreply.github.com>
2019-11-18 16:48:39 -0500

    Create tags.py

---
+++ learning paths/data analysis/bites/4/tags.py
@@ -0,0 +1,38 @@
+import os
+from collections import Counter
+import urllib.request
+
+# prep
+tempfile = os.path.join('/tmp', 'feed')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/feed',
+    tempfile
+)
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+
+import os
+from collections import Counter
+import urllib.request
+import re
+
+# prep
+tempfile = os.path.join('/tmp', 'feed')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/feed',
+    tempfile
+)
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+
+def get_pybites_top_tags(n=10):
+    """use Counter to get the top 10 PyBites tags from the feed
+       data already loaded into the content variable"""
+    TAG_category = re.compile(r'<category>([^<]+)</category>')
+    x = re.findall(TAG_category, content)
+    count = Counter(x)
+    return count.most_common(n)
