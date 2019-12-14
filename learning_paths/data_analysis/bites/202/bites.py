86219f027ab0a795f9ff28f860df966bad58aa3f
Ross Roberts <ross123roberts@gmail.com>
2019-12-14 17:16:40 -0500

    updated bites.py file to be successful

--- learning_paths/data_analysis/bites/202/bites.py
+++ learning_paths/data_analysis/bites/202/bites.py
@@ -2,6 +2,8 @@ import csv
 import os
 from pathlib import Path
 from urllib.request import urlretrieve
+import re
+
 
 data = 'https://bites-data.s3.us-east-2.amazonaws.com/bite_levels.csv'
 tmp = Path(os.getenv("TMP", "/tmp"))
@@ -10,18 +12,31 @@ stats = tmp / 'bites.csv'
 if not stats.exists():
     urlretrieve(data, stats)
 
+pattern = re.compile(r'.*Bite (\d*).+;(.*)$')
 
-def get_most_complex_bites(N=10, stats=stats):
+def get_most_complex_bites(N=6, stats=stats):
     """Parse the bites.csv file (= stats variable passed in), see example
        output in the Bite description.
        Return a list of Bite IDs (int or str values are fine) of the N
        most complex Bites.
     """
-    pass
+    bites = dict()
+    ret = list()
+    with open(stats) as fin:
+        reader = csv.reader(fin)
+        next(reader)
+        for line in reader:
+            if len(line) > 1:
+                line = ''.join(line)
+            line = str(line)
+            bites[re.sub(pattern, r'\1', line)] = re.sub(pattern, r'\2', line.strip("']"))
+    ordered_bites = {k: v for k, v in sorted(bites.items(), key=lambda item: item[1], reverse=True)}
+    for key, value in ordered_bites.items():
+        if len(ret) < N:
+            if value != 'None':
+                ret.append(key)
+    return ret
 
-
-for line in stats:
-    print(line)
 
 if __name__ == '__main__':
     res = get_most_complex_bites()
